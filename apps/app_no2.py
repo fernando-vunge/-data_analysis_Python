377da015c0ed9014ea50991063b3a871c0c0d0ba
Fernando Júlio Vunge <developervunge@gmail.com>
2024-02-25 20:13:08 +0000

    Add CSV files

--- apps/app_no2.py
+++ apps/app_no2.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plit
 import os
 
 NAME = "App Air Quality NO2"
-AIR_QUALITY_CSV_PATH = os.path.join(os.path.dirname(__file__), 'dataset', 'air_quality_no2.csv')
+AIR_QUALITY_CSV_PATH = os.path.join(os.path.dirname(__file__), '..' ,'datasets', 'air_quality_no2.csv')
 
 def get_name():
     return NAME
 
 def main():
     air_quality = pand.read_csv(AIR_QUALITY_CSV_PATH, index_col=0, parse_dates=True)
+    
     print(air_quality.head())
+    air_quality.plot()
+    plit.show()
