f6abbbbb68f3992845cefd81e7a19da0da64bd1e
Fernando Júlio Vunge <developervunge@gmail.com>
2024-02-25 20:05:35 +0000

    UPDATE

--- apps/app_no2.py
+++ apps/app_no2.py
@@ -1,11 +1,13 @@
 import pandas as pand
 import matplotlib.pyplot as plit
+import os
 
 NAME = "App Air Quality NO2"
+AIR_QUALITY_CSV_PATH = os.path.join(os.path.dirname(__file__), 'dataset', 'air_quality_no2.csv')
+
 def get_name():
     return NAME
 
 def main():
-    #self.air_quality = pand.read_csv('../datasets/air_quality_no2.csv', index_col=0, parse_dates=True)
-    print("ola")
-    #print(self.air_quality.head())
+    air_quality = pand.read_csv(AIR_QUALITY_CSV_PATH, index_col=0, parse_dates=True)
+    print(air_quality.head())
